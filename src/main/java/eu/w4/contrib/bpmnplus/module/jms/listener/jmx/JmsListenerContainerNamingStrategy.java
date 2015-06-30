079870d03df9b48498270d27de5d64825ccb4b29
Benjamin Cartereau <benjamin.cartereau@gmail.com>
2015-06-30 17:54:01 +0200

    Set a clean JMX naming strategy for message listener containers

--- src/main/java/eu/w4/contrib/bpmnplus/module/jms/listener/jmx/JmsListenerContainerNamingStrategy.java
+++ src/main/java/eu/w4/contrib/bpmnplus/module/jms/listener/jmx/JmsListenerContainerNamingStrategy.java
@@ -8,28 +8,59 @@ import org.springframework.jmx.export.naming.IdentityNamingStrategy;
 import static org.springframework.jmx.export.naming.IdentityNamingStrategy.TYPE_KEY;
 import org.springframework.jmx.support.ObjectNameManager;
 import org.springframework.util.ClassUtils;
+import org.springframework.util.StringUtils;
 
 /**
- *
+ * JmsListenerContainerNamingStrategy
+ * TODO : limit operations -> setConcurrency / maxConcurrency
  */
 public class JmsListenerContainerNamingStrategy extends IdentityNamingStrategy {
+
+  private static final String MESSAGE_LISTENER_CONTAINERS_DEFAULT_DOMAIN = "eu.w4.contrib.bpmnplus.module.jms";
+
+  private static final String DOUBLE_QUOTES = "\"";
 
   @Override
   public ObjectName getObjectName(Object managedBean, String beanKey) throws MalformedObjectNameException {
     ObjectName name = null;
+
     if (managedBean instanceof DefaultMessageListenerContainer) {
       DefaultMessageListenerContainer container = (DefaultMessageListenerContainer) managedBean;
-      String domain = "eu.w4.contrib.bpmnplus.module.jms";
+      String domain = MESSAGE_LISTENER_CONTAINERS_DEFAULT_DOMAIN;
       Hashtable<String, String> keys = new Hashtable<String, String>();
+      
+      // Type : container type
       keys.put(TYPE_KEY, ClassUtils.getShortName(managedBean.getClass()));
-      //keys.put(HASH_CODE_KEY, ObjectUtils.getIdentityHexString(managedBean));
+      
+      // Add destination...
       keys.put("destination", container.getDestinationName());
-      keys.put("selector", container.getMessageSelector());
-      return ObjectNameManager.getInstance(domain, keys);
+      
+      // ... and selector to distinguish them
+      if (!StringUtils.isEmpty(container.getMessageSelector())) {
+        keys.put("selector", escapeSelector(container.getMessageSelector()));
+      }
+      name = ObjectNameManager.getInstance(domain, keys);
     } else {
       name = super.getObjectName(managedBean, beanKey);
     }
+
     return name;
   }
 
+  /**
+   * Escape JMS selector so that it can be used in an ObjectName
+   * @param selector JMS selector
+   * @return String the escaped selector
+   * See also the method {@link javax.jms.Message}.
+   */
+  private String escapeSelector(String selector) {
+    StringBuilder escapedSelector = new StringBuilder(selector + 2);
+    if (selector.startsWith(DOUBLE_QUOTES) && selector.endsWith(DOUBLE_QUOTES)) {
+      escapedSelector.append(selector);
+    }
+    else {
+      escapedSelector.append(DOUBLE_QUOTES).append(selector).append(DOUBLE_QUOTES);
+    }
+    return escapedSelector.toString();
+  }
 }
