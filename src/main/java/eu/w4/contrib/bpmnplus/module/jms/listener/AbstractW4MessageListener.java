44b901db021e9187ff39e07ffd1bf2bf31d2e0bb
Benjamin Cartereau <benjamin.cartereau@gmail.com>
2016-04-26 15:09:05 +0200

    Always logout the principal even when an exception occurs

--- src/main/java/eu/w4/contrib/bpmnplus/module/jms/listener/AbstractW4MessageListener.java
+++ src/main/java/eu/w4/contrib/bpmnplus/module/jms/listener/AbstractW4MessageListener.java
@@ -24,8 +24,7 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.messaging.Message;
 
 /**
- * Abstract JMS messages listener and processor for W4. 
- * Should be thread safe.
+ * Abstract JMS messages listener and processor for W4. Should be thread safe.
  */
 public abstract class AbstractW4MessageListener implements InitializingBean {
 
@@ -49,69 +48,71 @@ public abstract class AbstractW4MessageListener implements InitializingBean {
   private Map<String, String> dataEntriesMapping;
 
   /**
-   * Handle received message.
-   * Please note that ByteMessage (without object mapping) and StreamMessage are not managed by the listener.
+   * Handle received message. Please note that ByteMessage (without object
+   * mapping) and StreamMessage are not managed by the listener.
+   *
    * @param message Spring generic message representation of the JMS message.
    * @param session The JMS Session if needed.
    * @param jmsMessage the original JMS message if needed.
    * @return String Message to send back or null if none
    */
   public String handle(Message<?> message, Session session, javax.jms.Message jmsMessage) {
-    if (message.getPayload() instanceof byte[] || message.getPayload() instanceof StreamMessage){
-      throw new IllegalArgumentException("Message payload type cannot be processed by this listener (" + message.getPayload().getClass().getName()+")");
+    if (message.getPayload() instanceof byte[] || message.getPayload() instanceof StreamMessage) {
+      throw new IllegalArgumentException("Message payload type cannot be processed by this listener (" + message.getPayload().getClass().getName() + ")");
     }
-    
+
     logger.debug("Received message: {}", message.getPayload().toString());
-    
+
     // Extract data
     Map<String, Object> dataEntries = mapPayloadToData(message.getPayload());
-    
+
     // Process W4 action
     String returnedMessage = processW4Action(message.getHeaders(), dataEntries);
-    
+
     return returnedMessage;
   }
-  
+
   /**
    * Map payload to a Map of data
+   *
    * @param payload Message payload to map
    * @return Map&lt;String, Object&gt; payload representation
    */
   private Map<String, Object> mapPayloadToData(Object payload) {
     // Build data entries map
     Map<String, Object> dataEntries = null;
-    
+
     if (dataEntryId != null) {
       dataEntries = new HashMap<String, Object>();
       if (payload instanceof String || payload instanceof Map) {
         dataEntries.put(dataEntryId, payload);
-      }
-      else {
+      } else {
         // Convert the object to a map (XSD type in W4)
         dataEntries.put(dataEntryId, convertObjectToMap(payload));
       }
-    }
-    else if (dataEntriesMapping != null) {
-      if (! (payload instanceof Map)) {
+    } else if (dataEntriesMapping != null) {
+      if (!(payload instanceof Map)) {
         throw new IllegalArgumentException("With dataEntries mappings defined, message payload should be of Map type.");
       }
       dataEntries = new HashMap<String, Object>();
       for (String mappingKey : dataEntriesMapping.keySet()) {
-        dataEntries.put(dataEntriesMapping.get(mappingKey), ((Map)payload).get(mappingKey));
+        dataEntries.put(dataEntriesMapping.get(mappingKey), ((Map) payload).get(mappingKey));
       }
     }
     return dataEntries;
   }
-  
+
   /**
    * Convert an object to a Map
+   *
    * @param object object to convert
    * @return Map&lt;String, Object&gt; converted object
    */
   protected Map<String, Object> convertObjectToMap(Object object) {
     Type objectType = TypeFactory.valueOf(object.getClass());
-    Type mapType = new TypeBuilder<Map<String, Object>>() {}.build();
-    
+    Type mapType = new TypeBuilder<Map<String, Object>>() {
+    }.build();
+
     MapperFactory mapperFactory = new DefaultMapperFactory.Builder().build();
     mapperFactory.classMap(objectType, mapType).byDefault().register();
     BoundMapperFacade boundMapper = mapperFactory.getMapperFacade(objectType, mapType);
@@ -127,40 +128,50 @@ public abstract class AbstractW4MessageListener implements InitializingBean {
    * <li>Do process the action as an authenticated user</li>
    * <li>Log out of the enfine</li>
    * </ul>
+   *
    * @param properties Set of properties if needed to process the action
    * @param dataEntries data that should (can) be used by the action
    * @return any message to send back
    */
   private String processW4Action(Map<String, Object> properties, Map<String, Object> dataEntries) {
-    // LOGIN
-    Principal principal = login();
-    
-    long timeBefore = System.currentTimeMillis();
-    
-    // PROCESS
-    String returnedMessage = doProcessW4Action(principal, properties, dataEntries);
-    
-    logger.debug("Message processed in {}ms", System.currentTimeMillis() - timeBefore);
-    
-    // LOGOUT
-    logout(principal);
-    
+    String returnedMessage = null;
+
+    Principal principal = null;
+    try {
+      // LOGIN
+      principal = login();
+
+      long timeBefore = System.currentTimeMillis();
+
+      // PROCESS
+      returnedMessage = doProcessW4Action(principal, properties, dataEntries);
+
+      logger.debug("Message processed in {}ms", System.currentTimeMillis() - timeBefore);
+    } finally {
+      // LOGOUT
+      if (principal != null) {
+        logout(principal);
+      }
+    }
+
     // Could return something (eg. process instance id) if needed. If return
     //  value is not null, reply-to channel will be used
     return returnedMessage;
   }
-  
+
   /**
    * Implements the W4 action to process as the authenticated Principal
+   *
    * @param principal Authenticated Principal
    * @param properties Set of properties if needed to process the action
    * @param dataEntries data that should (can) be used by the action
    * @return anything to send back to the emitter (null if nothing to return)
    */
   protected abstract String doProcessW4Action(Principal principal, Map<String, Object> properties, Map<String, Object> dataEntries);
-  
+
   /**
    * Get W4 process engine service
+   *
    * @return ProcessService W4 process service
    */
   final protected ProcessService getProcessService() {
@@ -176,9 +187,10 @@ public abstract class AbstractW4MessageListener implements InitializingBean {
     }
     return processService;
   }
-  
+
   /**
    * Log on to W4 BPMN+ Engine
+   *
    * @return Principal authenticated user principal
    */
   private Principal login() {
@@ -218,17 +230,18 @@ public abstract class AbstractW4MessageListener implements InitializingBean {
    * <li>define the definitionsIdentifier</li>
    * </ul>
    * Can be overridden if others initializations must be done
+   *
    * @throws Exception an error occured
    */
   @Override
   //@PostConstruct
   public void afterPropertiesSet() throws Exception {
     //Assert.notNull(engineService, "EngineService must be set");
-    assert engineService!=null : "EngineService must be set";
-    assert engineLogin!=null : "Login must be set";
-    assert enginePassword!=null : "Password must be set";
-    assert definitionsIdentifierName!=null : "Definitions identifier must be set";
-    
+    assert engineService != null : "EngineService must be set";
+    assert engineLogin != null : "Login must be set";
+    assert enginePassword != null : "Password must be set";
+    assert definitionsIdentifierName != null : "Definitions identifier must be set";
+
     try {
       ObjectFactory factory = engineService.getObjectFactory();
       this.definitionsIdentifier = factory.newDefinitionsIdentifier();
@@ -241,9 +254,10 @@ public abstract class AbstractW4MessageListener implements InitializingBean {
       throw new JMSModuleException("Cannot retrieve identifiers", rex);
     }
   }
-  
+
   /**
    * Setter for W4 BPMN+ Engine service
+   *
    * @param engineService W4 engine service
    */
   public void setEngineService(EngineService engineService) {
